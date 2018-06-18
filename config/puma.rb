32b4ca121781fa1def3a452ce0a4bd85a4914215
tra <terasute0321@gmail.com>
2018-06-19 08:54:08 +0900

    puma ホームディレクトリ変更

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,7 @@
 _proj_path = "#{File.expand_path("../..", __FILE__)}"
 _proj_name = File.basename(_proj_path)
-_home = ENV.fetch("HOME") { "/var" }
+#_home = ENV.fetch("HOME") { "/var" }
+_home = "/var"
 pidfile "#{_home}/run/#{_proj_name}.pid"
 bind "unix://#{_home}/run/#{_proj_name}.sock"
 directory _proj_path
