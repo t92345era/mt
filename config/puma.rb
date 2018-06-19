3eded2bb6b585701c7946461e882d5d6d2e7b2d5
tra <terasute0321@gmail.com>
2018-06-19 11:27:15 +0900

    puma.rb 修正

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,9 @@
 _proj_path = "#{File.expand_path("../..", __FILE__)}"
 _proj_name = File.basename(_proj_path)
-_home = ENV.fetch("HOME") { "/var" }
-#_home = "/var"
-pidfile "#{_home}/run/rails_#{_proj_name}.pid"
-bind "unix://#{_home}/run/rails_#{_proj_name}.sock"
+#_home = ENV.fetch("HOME") { "/var" }
+_home = "/var"
+pidfile "#{_home}/run/puma/rails_#{_proj_name}.pid"
+bind "unix://#{_home}/run/puma/rails_#{_proj_name}.sock"
 directory _proj_path
 
 # Puma can serve each request in a thread from an internal thread pool.
